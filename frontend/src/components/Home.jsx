0646db27e381d0233356a6fc96f29bc5a552ce3b
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-19 15:52:46 -0500

    Add components to the frontend part

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar(props) {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  )
+}
