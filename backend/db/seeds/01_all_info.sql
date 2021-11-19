0972a574761798f37458d036f5c9fd5101c3f398
Emily K <emily.ke@hotmail.com>
2021-11-19 18:15:49 -0500

    fixed database tables, good to seed

--- backend/db/seeds/01_all_info.sql
+++ backend/db/seeds/01_all_info.sql
@@ -1,11 +1,14 @@
-INSERT INTO users (name, email, password, avatar)
+INSERT INTO users (name, email, password, avatar_url)
 values( )
 
-INSERT INTO income (id, income_date, income_type, income_desc, income_amt, user_id)
+INSERT INTO income_type (id, income_desc)
 values( )
 
-INSERT INTO categories (id, category_type)
+INSERT INTO income (id, date_created, income_type_id, income_desc, income_amt, user_id)
 values( )
 
-INSERT INTO expenses (id, expense_date, expense_type, expense_amt, user_id, category_id)
+INSERT INTO expenses_type (id, expense_desc)
+values( )
+
+INSERT INTO expenses (id, expense_date, expense_amt, user_id, expenses_type)
 values( )
