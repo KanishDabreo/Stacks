6c63aae559f377f4418a3d79619449fdb4892100
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-11-20 19:47:42 +0000

    testing route

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
+const PORT = process.env.PORT || 8080;
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+
+const indexRouter = require("./routes/users");
 
 const app = express();
 
@@ -15,15 +18,24 @@ const usersRouter = require("./routes/users");
 const expensesRouter = require("./routes/expenses");
 const incomesRouter = require("./routes/incomes");
 const categoriesRouter = require("./routes/categories");
+const authenticateRouter = require("./routes/authenticate");
 
+// app.use("/api/users", usersRouter);
+// app.use("/api/expenses", expensesRouter);
+// app.use("/api/incomes", incomesRouter);
+// app.use("/api/categories", categoriesRouter);
 
-app.use("/api/users", usersRouter);
-app.use("/api/expenses", expensesRouter);
-app.use("/api/incomes", incomesRouter);
-app.use("/api/categories", categoriesRouter);
+// app.use("/api/authenticate", authenticateRouter);
+// app.get("/api/authenticate");
+// app.post("/api/login");
+// app.post("/api/register");
 
-app.get("/api/authenticate");
-app.post("/api/login");
-app.post("/api/register");
+app.get("/",(req, res) => {
+  res.send("hello world")
+})
 
 module.exports = app;
+
+app.listen(PORT,"0.0.0.0", () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
