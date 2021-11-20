ebff47bbf6d4d1d2d5078e1d0d1cb72b887b6177
Emily K <emily.ke@hotmail.com>
2021-11-20 16:55:26 -0500

    added react-router to route different pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import AddExpenses from './components/AddExpenses';
+import { Routes, Route, Link } from "react-router-dom";
+import Dashboard from './components/Dashboard';
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <AddExpenses/>
-      Hello world
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+      </nav>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
