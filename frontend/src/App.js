818a86e983f939e1bf28e62ce1db9d1525a1c2b3
Emily K <emily.ke@hotmail.com>
2021-11-20 14:41:25 -0500

    added nodemon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import AddExpenses from './components/AddExpenses';
 
 function App() {
   return (
     <div className="App">
+      <AddExpenses/>
+      Hello world
     </div>
   );
 }
