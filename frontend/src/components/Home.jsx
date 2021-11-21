87780ac636d9217e3e4675fb4728302113114917
Emily K <emily.ke@hotmail.com>
2021-11-20 23:44:38 -0500

    finished css + layout of homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,12 @@
+import './home.css';
+
 export default function Home(props) {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div class="homepage">
+      <h4 class="optimize">Optimize</h4>
+      <h4 class="your">your</h4>
+      <h4 class="spending">spending.</h4>
+      <img class="homepage-logo" src="./static/images/homepage_logo.png"/>
     </div>
   )
 }
