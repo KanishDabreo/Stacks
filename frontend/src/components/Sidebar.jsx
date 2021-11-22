82966576d458e3ef33afbe7a866af49d402ae743
Kanisha Dabreo <kanishaddabreo@gmail.com>
2021-11-22 18:45:10 +0000

    sidebar adjustment.. remove duplicates

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-// import * as Icons from "react-icons/ai"; 
-// import * as FaIcons from "react-icons/fa";
-import { FaHome, FaChartPie } from "react-icons/fa";
 import "./Sidebar.css";
 import { Link } from 'react-router-dom'
 import { SidebarInput } from './SidebarInput';
@@ -10,8 +7,6 @@ export default function Sidebar(props) {
   return (
     <div className="sidebar">
       <Link to="#" className="sidebar_menu">
-      <FaHome/>
-      <FaChartPie/>
       </Link>
       <li>
 
