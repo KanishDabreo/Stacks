aa8523d8f16ff9dad3b5cd851840b26a29e820a1
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-22 11:41:52 -0500

    fix: add avatars to user seeds

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name, email, password, avatar_url)
 VALUES
-('Bob', 'bob.cool@gmail.com', 'bob_test', 'url'),
-('John', 'john,smith@gmail.com', 'john_test', 'url'),
-('Aria', 'aria.mom@gmail.com', 'aria_test', 'url'),
-('Tom', 'tom.kruz@gmail.com', 'tom_test', 'url')
+('Bob', 'bob.cool@gmail.com', 'bob_test', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/bob.png'),
+('John', 'john.smith@gmail.com', 'john_test', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/john.png'),
+('Aria', 'aria.mom@gmail.com', 'aria_test', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/aria.png'),
+('Tom', 'tom.kruz@gmail.com', 'tom_test', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/tom.png')
