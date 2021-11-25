2e3626f6beeb9907c80943dca54b9be3321eecde
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-24 19:29:55 -0500

    Implement login feature and pass data into frontend

--- backend/routes/authenticate.js
+++ backend/routes/authenticate.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const cookieSession = require('cookie-session');
+
 
 module.exports = (db) => {
-
-  router.use(cookieSession({
-    name: 'session',
-    keys: ['key1', 'key2'],
-  }));
 
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
@@ -25,17 +20,24 @@ module.exports = (db) => {
 
   router.post("/register", async (req, res) => {
 
-    const {name, email, password, confirmPassword } = req.body;
+    const { name, email, password, confirmPassword } = req.body;
+
+    let errors = [];
+
+    if (!name || !email || !password || !confirmPassword) {
+      errors.push({ message: "Please, fill out all fields." });
+    }
 
     if (password !== confirmPassword) {
-      res.send('Password confirmation does not match');
+      errors.push({ message:'Passwords do not match.' });
       return;
     } else {
       try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+        // const salt = await bcrypt.genSalt(10);    
+        // const hashedPassword = await bcrypt.hash(password, salt);
 
         let queryString = `INSERT INTO users (name, email, password, avatar_url) VALUES ($1, $2, $3, $4) RETURNING *`;
-        let queryParams = [name, email, hashedPassword, 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png'];
+        let queryParams = [ name, email, password, 'https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png' ];
 
         return db
           .query(queryString, queryParams)
@@ -48,22 +50,35 @@ module.exports = (db) => {
 
   router.post("/login", async (req, res) => {
     console.log(req.body);
-    const { email, password } = req.body;
-    const user = users.find(user => user.email == email);
 
-    // Check if user exists in DB
-    if (!user) {
-      return res.status(400).send('User does not exist');
-    }
-    try {
-      if (await bcrypt.compare(password, user.password)) {
-        res.send("Success");
-      } else {
-        res.send("Invalid username or password");
-      }
-    } catch (error) {
-      res.status(500).send();
-    }
+    const { email, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    
+    // const hashedPassword = await bcrypt.hash(password, salt);
+    // console.log(hashedPassword);
+    db.query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then(async data => {
+      console.log(data.rows[0].password);
+      const user = data.rows[0];
+      const password = user.password;
+      console.log(user.name);
+      // Check if user exists in DB        
+        if (!user) {
+          return res.status(400).send('User does not exist');
+        } else {
+          try {
+            if (user.password === password) {
+              res.send(user.name);
+            } else {
+              res.send("Invalid username or password");
+            }
+          } catch (error) {
+            res.status(500).send();
+          }
+        }   
+      })
+
   });
 
   return router;
