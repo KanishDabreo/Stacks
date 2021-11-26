22dd8dacf1c48be647a1c7834685601d7615d2a8
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-26 10:26:41 -0500

    Change seeds for user table, change names and password to be hashed

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name, email, password, avatar_url)
 VALUES
-('Bob', 'bob.cool@gmail.com', 'bob_test', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/bob.png'),
-('John', 'john.smith@gmail.com', 'john_test', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/john.png'),
-('Aria', 'aria.mom@gmail.com', 'aria_test', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/aria.png'),
-('Tom', 'tom.kruz@gmail.com', 'tom_test', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/tom.png')
+('Bob Cool', 'bob.cool@gmail.com', '$2b$10$1C.a2TWw9yJQBmAfzxZFGuzUH1OER6ftwIyp2EDv3NYk7tuWgsR1a', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/bob.png'),
+('John Smith', 'john.smith@gmail.com', '$2b$10$1C.a2TWw9yJQBmAfzxZFGuzUH1OER6ftwIyp2EDv3NYk7tuWgsR1a', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/john.png'),
+('Aria Gold', 'aria.mom@gmail.com', '$2b$10$1C.a2TWw9yJQBmAfzxZFGuzUH1OER6ftwIyp2EDv3NYk7tuWgsR1a', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/aria.png'),
+('Tom Kruz', 'tom.kruz@gmail.com', '$2b$10$1C.a2TWw9yJQBmAfzxZFGuzUH1OER6ftwIyp2EDv3NYk7tuWgsR1a', 'https://raw.githubusercontent.com/KanishDabreo/finals/main/frontend/public/static/images/tom.png')
