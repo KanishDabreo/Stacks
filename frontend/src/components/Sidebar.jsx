ad3c78fb71b506f878383afebe47e4a4f56f6c0c
Emily K <emily.ke@hotmail.com>
2021-11-28 21:01:17 -0500

    resolve conflict

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -58,7 +58,7 @@ export default function Sidebar({isLoggedIn}) {
           className="sidebar-item"
           onClose={handleTooltipClose} 
           onOpen={handleTooltipOpen}    
-          title="Add Expense">
+          title="Add Expense/Income">
           <Link to="/expenses"><h2><RiAddBoxFill /></h2></Link>
         </Tooltip>
       </div>
